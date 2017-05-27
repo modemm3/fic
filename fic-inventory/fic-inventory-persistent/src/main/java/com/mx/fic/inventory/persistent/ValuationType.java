d03fe9230b2bfe6b6db30522a8da9167527db33b
developer <developer@MacBook-Pro-de-esperanza.local>
2017-05-27 17:49:16 -0500

    Se modifica la query

---
+++ fic-inventory/fic-inventory-persistent/src/main/java/com/mx/fic/inventory/persistent/ValuationType.java
@@ -0,0 +1,15 @@
+package com.mx.fic.inventory.persistent;
+
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/*@Entityntity
+@Table (name="valuation_type")
+@NamedQueries({
+	@NamedQueryry(name = "ValuationType.getAllByCompany", query= "select vt from ValuationType vt where vt.company.id=:id")
+})
+public class ValuationType {
+
+}*/
