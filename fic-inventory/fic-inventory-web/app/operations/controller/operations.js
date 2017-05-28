c755618cce2d7ca40ce71aade6987236cde02a35
mode <modestom3@gmail.com>
2017-05-27 20:43:23 -0500

    se consume el ws de unidades de medida

--- fic-inventory/fic-inventory-web/app/operations/controller/operations.js
+++ fic-inventory/fic-inventory-web/app/operations/controller/operations.js
@@ -4,6 +4,13 @@
 'use strict';
 angular.module('app.controller').
 controller('operationController', ['$scope','OperationServices',function($scope,OperationServices){
-	$scope.measureUnity=['Kilo','Litros'];
-	OperationServices.OperationList(1);
+	$scope.measureUnitId="-1";
+	$scope.measureUnitName = "Unidad de Medida";
+	OperationServices.OperationList(1).success(function(responseDTO){
+		$scope.measureUnits = responseDTO.measureUnitDTOLst;
+	});
+	$scope.measureUnitId=function(id){
+		return id;
+	}
+	
 }]);
