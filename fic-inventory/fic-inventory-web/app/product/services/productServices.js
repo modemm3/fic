c97b2812b2bd1d1ac351ab1b420796c55b5e1d19
mode <modestom3@gmail.com>
2017-05-31 02:29:17 -0500

    se agregan cambios para el consumo del servicio en la tabla de operaciones

---
+++ fic-inventory/fic-inventory-web/app/product/services/productServices.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+'use strict';
+	angular.module('app.services')
+		.factory('ProductServices',['$resource','urlBase','$http',function($resource,urlBase,$http){
+			return {
+				getProducts:function(companyId){
+					return $http({method: 'POST',url:urlBase+'product/getProductByCompany/'+companyId,
+						headers:{'Content-Type':'application/json'}})
+					}
+				};
+		}]);
