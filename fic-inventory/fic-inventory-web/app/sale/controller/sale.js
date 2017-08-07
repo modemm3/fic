56633f5d27de380cd1ec08fd29635f5bbcadcc5d
root <root@debian.debian>
2017-08-06 23:20:43 -0500

    se agrega el formulario de ventas y la configuracion de idiomas

---
+++ fic-inventory/fic-inventory-web/app/sale/controller/sale.js
@@ -0,0 +1,15 @@
+/**
+ * Controler para la carga de inventario inicial
+ */
+'use strict';
+angular.module('app.controller').
+controller('saleController', ['$scope',function($scope){
+
+//	console.log('inicializando Sale');
+//	var elems = document.getElementsByClassName('content');
+//
+//	for(var i = 0; i != elems.length; ++i) {
+//		elems[i].style.visibility = "hidden"; // hidden has to be a string
+//	}
+	
+}]);
