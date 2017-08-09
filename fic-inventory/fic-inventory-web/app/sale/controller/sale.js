b374af5ba2f60fd091ae25171c0f0724cb366fd5
mmejia <mmejia@debian.debian>
2017-08-08 23:23:36 -0500

    se agregan eventos al boton de agregar y el evento enter a la caja de texto

--- fic-inventory/fic-inventory-web/app/sale/controller/sale.js
+++ fic-inventory/fic-inventory-web/app/sale/controller/sale.js
@@ -2,9 +2,39 @@
  * Controler para la carga de inventario inicial
  */
 'use strict';
-angular.module('app.controller').
-controller('saleController', ['$scope',function($scope){
+angular.module('app.controller').controller('saleController', [ '$scope', function($scope) {
 
+	$scope.tickets = [];
+	$scope.ticket = {
+		"code" : "",
+		"description" : "",
+		"salePrice" : "",
+		"stock" : "",
+		"price" : "",
+		"amount" : ""
+	}
+	$scope.add = function() {
+		var item = {
+			"code" : $scope.ticket.code,
+			"description" : "Description",
+			"salePrice" : "",
+			"stock" : "",
+			"price" : "",
+			"amount" : ""
+		}
+		$scope.tickets.push(item);
+		$scope.init();
+	}
+
+	$scope.keyAdd = function(keyEvent){
+		if (keyEvent.keyCode == 13) {
+	        $scope.add();
+	    }
+		
+	}
+	$scope.init = function(){
+		$scope.ticket = {};
+	}
 //	console.log('inicializando Sale');
 //	var elems = document.getElementsByClassName('content');
 //
@@ -52,4 +82,4 @@ controller('saleController', ['$scope',function($scope){
 //		"amount":""
 //	}	
 //	]
-}]);
+} ]);
