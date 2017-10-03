5762479983938dcaa3e51893dbd6ba2fac5a846c
mode <modestom3@gmail.com>
2017-10-02 22:58:32 -0500

    se agregan cambios de tipos de movimientos

--- fic-inventory/fic-inventory-web/app/movementConcept/controller/movementConcept.js
+++ fic-inventory/fic-inventory-web/app/movementConcept/controller/movementConcept.js
@@ -2,24 +2,24 @@
  * Controler para la carga de inventario inicial
  */
 'use strict';
-angular.module('app.controller').
-controller('MovementConceptController', ['$scope','MovementConceptServices',function($scope,MovementConceptServices){
-	
-	$scope.movementConceptList=[{}];
-	$scope.movementConcetp={
-			id:'',
-			name:'',
-			description:'',
-			companyId:'1'
-				
+angular.module('app.controller').controller('MovementConceptController', [ '$scope', 'MovementConceptServices', '$compile', function($scope, MovementConceptServices, $compile) {
+
+	$scope.itemsByPage = 3;
+	$scope.movementConceptList = [ {} ];
+	$scope.movementConcept = {
+		id : '',
+		name : '',
+		description : '',
+		companyId : '1',
+		statusId:'1'
 	};
-	$scope.movementConceptResponse={
-			message:{
-				code:"",
-				message:""
-			}
+	$scope.movementConceptResponse = {
+		message : {
+			code : "",
+			message : ""
+		}
 	};
-	$scope.show=false;
+	$scope.show = false;
 	$scope.init = function() {
 		MovementConceptServices.getMovementConcetpByCompany(1).success(function(response) {
 			$scope.movementConceptResponse.message.code = response.message.code;
@@ -29,15 +29,48 @@ controller('MovementConceptController', ['$scope','MovementConceptServices',func
 			}
 		}
 		);
-	};	
-	$scope.save = function(){
-		MovementConceptServices.saveMeasureUnit($scope.movementConcetp).success(function(response){
-			$scope.movementConceptResponse.message.code=response.message.code;
-			if($scope.movementConceptResponse.message.code===200){
-				$scope.show=true;
-				$scope.movementConceptResponse.message.message=response.message.message;
+	};
+	$scope.initValues = function() {
+		$scope.movementConcept = {
+			id : '',
+			name : '',
+			description : '',
+			companyId : '1',
+			statusId:'1'
+		};
+		$("#d").empty();
+	}
+	$scope.save = function() {
+		MovementConceptServices.saveMeasureUnit($scope.movementConcept).success(function(response) {
+			$scope.movementConceptResponse.message.code = response.message.code;
+			if ($scope.movementConceptResponse.message.code === 200) {
+				$scope.show = true;
+				$scope.movementConceptResponse.message.message = response.message.message;
 			}
 		});
 	}
+	$scope.add = function(data) {
+		$scope.initValues();
+		var compiledeHTML = $compile("<div movcon class='row text-center'></div>")($scope);
+		$("#d").append(compiledeHTML);
+		$("#d").modal('show');
+
+	}
+	$scope.edit = function(data) {
+		$scope.movementConcept.id = data.id;
+		$scope.movementConcept.name = data.name;
+		$scope.movementConcept.description = data.description;
+		$scope.movementConcept.companyId = data.companyId;
+		var compiledeHTML = $compile("<div movcon class='row text-center'></div>")($scope);
+		$("#d").empty();
+		$("#d").append(compiledeHTML);
+		$("#d").modal('show');
+	};
 	$scope.init();
-}]);
+} ]).directive('movcon', function() {
+	return {
+		templateUrl : "movementConcept/views/movementConcept.html"
+	//			templateUrl:"company/views/company.html"
+	};
+
+});
